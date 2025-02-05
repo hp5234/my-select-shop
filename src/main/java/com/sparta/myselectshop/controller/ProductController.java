4274377e54096045a64e21737ab1368a778b8857
jeonjinwoo <wls45al@gmail.com>
2025-02-05 15:38:56 +0900

    관심상품 조회 API 추가

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,12 +7,16 @@ import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 public class ProductController {
 
     private final ProductService productService;
+
+    public static final int MIN_MY_PRICE = 100;
 
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
@@ -21,6 +25,17 @@ public class ProductController {
 
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable int id, @RequestBody ProductMypriceRequestDto requestDto) {
+
+        int myprice = requestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해주세요." );
+        }
+
         return productService.updateProduct(id, requestDto);
+    }
+
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        return productService.getProducts();
     }
 }
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -9,13 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
-
-    public static final int MIN_MY_PRICE = 100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = productRepository.save(new Product(requestDto));
@@ -24,17 +24,18 @@ public class ProductService {
 
     @Transactional
     public ProductResponseDto updateProduct(int id, ProductMypriceRequestDto requestDto) {
-
-        int myprice = requestDto.getMyprice();
-        if (myprice < MIN_MY_PRICE) {
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해주세요." );
-        }
-
         Product product = productRepository
                 .findById(id)
                 .orElseThrow(() -> new NullPointerException("해당 상품을 찾을 수 없습니다."));
 
         product.update(requestDto);
         return new ProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponseDto::new)
+                .toList();
     }
 }
