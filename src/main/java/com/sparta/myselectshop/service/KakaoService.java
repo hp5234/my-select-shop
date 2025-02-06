d832e84ae5a99e931769cc42f10ed7091b62d32d
jeonjinwoo <wls45al@gmail.com>
2025-02-06 12:11:58 +0900

    카카오 API : 사용자 정보를 통한 회원가입 구현 + MYSQL 로 복귀

--- src/main/java/com/sparta/myselectshop/service/KakaoService.java
+++ src/main/java/com/sparta/myselectshop/service/KakaoService.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.myselectshop.dto.KakaoUserInfoDto;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.jwt.JwtUtil;
 import com.sparta.myselectshop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @Slf4j(topic = "KAKAO Login")
 @Service
@@ -42,7 +45,13 @@ public class KakaoService {
         // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
         KakaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
 
-        return null;
+        // 3. 필요시 회원가입 처리
+        User kakaoUser = registerKakaoUserIfNeeded(kakaoUserInfo);
+
+        // 4. JWT 토큰 반환
+        String createToken = jwtUtil.createToken(kakaoUser.getUsername(), kakaoUser.getRole());
+
+        return createToken.substring(7);
     }
 
     private String getToken(String code) throws JsonProcessingException {
@@ -129,6 +138,36 @@ public class KakaoService {
 
         log.info("카카오 사용자 정보: " + id + ", " + nickname + ", " + email);
         return new KakaoUserInfoDto(id, nickname, email);
+    }
+
+    private User registerKakaoUserIfNeeded(KakaoUserInfoDto kakaoUserInfo) {
+        // DB 에 중복된 Kakao Id 가 있는지 확인
+        Long kakaoId = kakaoUserInfo.getId();
+        User kakaoUser = userRepository.findByKakaoId(kakaoId).orElse(null);
+
+        if (kakaoUser != null) {
+            return kakaoUser;
+        }
+
+        // 카카오 사용자 email 동일한 email 가진 회원이 있는지 확인
+        String kakaoEmail = kakaoUserInfo.getEmail();
+        User sameEmailUser = userRepository.findByEmail(kakaoEmail).orElse(null);
+
+        if (sameEmailUser == null) {
+            // 신규 회원가입
+            // password: random UUID
+            String password = UUID.randomUUID().toString();
+            String encodedPassword = passwordEncoder.encode(password);
+
+            kakaoUser = new User(kakaoUserInfo.getNickname(), encodedPassword, kakaoEmail, UserRoleEnum.USER, kakaoId);
+        } else {
+            kakaoUser = sameEmailUser;
+            // 기존 회원정보에 카카오 Id 추가
+            kakaoUser = kakaoUser.kakaoIdUpdate(kakaoId);
+        }
+
+        return userRepository.save(kakaoUser);
+
     }
 
 
