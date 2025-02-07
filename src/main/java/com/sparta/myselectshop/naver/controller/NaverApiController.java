e83bed9a1f79c8cff2b4a9ab68ab688c8ac6194a
jeonjinwoo <wls45al@gmail.com>
2025-02-07 10:33:28 +0900

    Top5 회원 찾기 : 수행시간 누적

--- src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
+++ src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
@@ -4,6 +4,7 @@ package com.sparta.myselectshop.naver.controller;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.naver.service.NaverApiService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -19,7 +21,8 @@ public class NaverApiController {
     private final NaverApiService naverApiService;
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestParam String query)  {
+    public List<ItemDto> searchItems(@RequestParam("query") String query)  {
+        log.info("[/search] : 컨트롤러 진입");
         return naverApiService.searchItems(query);
     }
 }
